9d4c0d7898934322026b4ab4b950c02506a4005c
ringomag <optimuskrajm@gmail.com>
2021-03-10 12:56:33 +0100

    with search and inline form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 def home(request):
     orders = Order.objects.all()
@@ -30,19 +32,26 @@ def customer(request, pk):
     orders= customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count, 'myFilter':myFilter}
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'), extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer) # removes old orders from instance
+    #form = OrderForm(initial={'customer':customer})
     if request.method=="POST":
         #print("printig post", request.POST)
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
@@ -63,6 +72,6 @@ def deleteOrder(request, pk):
     if request.method=='POST':
         order.delete()
         return redirect('/')
-        
+
     context = {'item':order}
     return render(request, 'accounts/delete.html', context)
